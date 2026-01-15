f80a4802dd7a731c69c5bfa1bc21ae5bf4b54c66
Poulami Saha <poulami.b.saha@accenture.com>
2026-01-15 12:36:09 +0530

    width changed

--- Frontend/nimbus-frontend/src/components/Sidebar.tsx
+++ Frontend/nimbus-frontend/src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 function Sidebar() {
     return(
-      <aside style={{ width: "200px", background: "#f4f4f4", padding: "1rem" }}>
+      <aside style={{ width: "500px", background: "#f4f4f4", padding: "1rem" }}>
       <p>Dashboard</p>
       <p>Projects</p>
       <p>Tasks</p>
